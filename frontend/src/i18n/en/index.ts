352242a15bec0750782bfa6e9f59e848997d27fe
Jacqueline Medley <jmedley60@hotmail.com>
2025-04-13 23:26:46 -0400

    Update i18n index.ts

--- frontend/src/i18n/en/index.ts
+++ frontend/src/i18n/en/index.ts
@@ -1,12 +1,23 @@
 import type { BaseTranslation } from '../i18n-types'
 
 const en = {
-	// TODO: your translations go here
-		
+	// TODO: your translations go here		
 	title: 'Distance Calculator',
-	
-	
-	HI: 'Hi {name:string}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n',
+	calcDescription: 'Prototype wev application for calculating the distance between addresses.',
+	source: 'Source Address',
+	destination: 'Destination Address',
+	unit: 'Unit',
+	distance: 'Distance',
+	calcDistance: 'Calculate Distance',
+	viewHist: 'View Historical Queries',
+	miles: 'Miles',
+	kilo: 'Kilometers',
+	both: 'Both',
+	histQueries: 'Historical Queries',
+	histDescription: "History of user's queries.",
+	distMiles: 'Distance in Miles',
+	distKilo: 'Distance in Kilometers',
+	backToCalc: 'Back to Calculator',
 } satisfies BaseTranslation
 
 
