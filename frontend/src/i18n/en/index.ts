e18757965e38c335f63fbe041c04d4335609b676
Jacqueline Medley <jmedley60@hotmail.com>
2025-04-13 22:28:50 -0400

    Added i18n Internationalization

---
+++ frontend/src/i18n/en/index.ts
@@ -0,0 +1,13 @@
+import type { BaseTranslation } from '../i18n-types'
+
+const en = {
+	// TODO: your translations go here
+		
+	title: 'Distance Calculator',
+	
+	
+	HI: 'Hi {name:string}! Please leave a star if you like this project: https://github.com/ivanhofer/typesafe-i18n',
+} satisfies BaseTranslation
+
+
+export default en
