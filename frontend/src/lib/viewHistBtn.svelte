0b7075c9a8e76cd3461d047ce8e945aaf69d585b
Jacqueline Medley <jmedley60@hotmail.com>
2025-04-14 12:32:15 -0400

    Added view historical button component

---
+++ frontend/src/lib/viewHistBtn.svelte
@@ -0,0 +1,30 @@
+<script>
+    import {LL} from "../i18n/i18n-svelte"
+    import viewedImg from '$lib/images/viewed_img.png';
+</script>
+
+<button>{$LL.viewHist()} <img src={viewedImg} alt="recentlyViewedImg" /></button>
+
+
+<style>
+    button{
+        float: right;
+        margin-top: -85px;
+        padding: 12px 13px 12px 13px;
+        background-color: #000000;
+        color: #FFFFFF;
+        font-family: 'Inter', sans-serif;
+        font-weight: 400;
+        font-size: 14px;
+        line-height: 18px;
+        letter-spacing: 0.16px;
+    }
+
+    img{
+        height: 12px;
+        width: 14px;
+        margin-top: 1px;
+        margin-left: 2px;
+
+    }
+</style>
